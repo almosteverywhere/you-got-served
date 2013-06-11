da3137420c2b47bb16243575328f6bb31ab57a68
Julie Lavoie <julielavoie@Julies-MacBook-Pro-2.local>
2013-06-11 13:23:44 -0400

    First commit.

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+http.createServer(function(request, response) {
+
+  console.log(request.url);
+
+  var filePath = request.url;
+  if (filePath === "/"){
+      filePath = "./index.html";
+  }
+  filePath = '.' + filePath
+  console.log("true or false" + fs.existsSync(filePath));
+  if (fs.existsSync(filePath)){
+
+    var file = fs.readFileSync(filePath, "utf8");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(file);
+
+  }
+
+  else {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write("doesn't exist");
+  }
+
+  response.end();
+}).listen(8888);
+
+console.log("Server has started.");
